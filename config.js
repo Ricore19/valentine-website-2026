74a0ea30ff73b857e343d7ef38ec65a785d487e6
Ricore19 <nvandaniel@gmail.com>
2026-01-29 16:21:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dcfxkit3c/video/upload/v1769700047/Kina_Grannis_-_Can_t_Help_Falling_In_Love_From_Crazy_Rich_Asians__mp3.pm_zob257.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
